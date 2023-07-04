d6f8acb0fefdef776467ac6bf5a1ea2c47f61747
Dillon Boardman <130460469+dillonboardman-opencredo@users.noreply.github.com>
2023-07-04 11:08:13 +0100

    Add fourth kata

---
+++ 4./src/main/java/com/opencredo/kata/serialization/Serialized.java
@@ -0,0 +1,30 @@
+package com.opencredo.kata.serialization;
+
+import java.io.*;
+import java.time.ZonedDateTime;
+
+public class Serialized implements Serializable {
+    private long serialisationDate;
+    private transient ZonedDateTime rehydrationDate;
+
+    public long serialisationDate() {
+        return this.serialisationDate;
+    }
+
+    public ZonedDateTime rehydrationDate() {
+        return this.rehydrationDate;
+    }
+
+    @Serial
+    private void writeObject(java.io.ObjectOutputStream stream) throws IOException {
+        stream.defaultWriteObject();
+        stream.writeLong(ZonedDateTime.now().toEpochSecond());
+    }
+
+    @Serial
+    private void readObject(java.io.ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        stream.defaultReadObject();
+        serialisationDate = stream.readLong();
+        rehydrationDate = ZonedDateTime.now();
+    }
+}
